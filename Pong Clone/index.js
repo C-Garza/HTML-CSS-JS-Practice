33d52383d646ec1838d1db33bf57e3c5a2ebe720
Christian Garza <16683618+C-Garza@users.noreply.github.com>
2019-10-23 00:34:49 -0700

    Add main and score menu and limit fps to 60

--- Pong Clone/index.js
+++ Pong Clone/index.js
@@ -3,9 +3,22 @@ window.onload = function() {
   window.webkitRequestAnimationFrame ||
   window.mozRequestAnimationFrame ||
   function(callback) {window.setTimeout(callback, 1000/60)};
+  let keysDown = [];
+  let isKeyDown = false;
   ////GAME VARIABLES
-  let BALL_SPEED_X = 0;
-  let BALL_SPEED_Y = -3;
+  let BALL_SPEED_X = -6;
+  let BALL_SPEED_Y = 0;
+  let BALL_DEACCELERATION = 1;
+  let PADDLE_SPEED_Y = 10;
+  let COMP_PADDLE_SPEED_Y = 5;
+  let COMP_PADDLE_SPEED_Y_MULTIPLYER = 1.6;
+  let COMP_CHALLENGE = false;
+  let REALISTIC_PADDLE = false;
+  let FINAL_SCORE = 5;
+  let GAME_FINISH = false;
+  if(BALL_DEACCELERATION <= 0) {
+    BALL_DEACCELERATION = 1;
+  }
 
   ////PADDLE OBJECT
   let Paddle = {
@@ -16,7 +29,8 @@ window.onload = function() {
         x: whichSide === "left" ? 75 : this.canvas.width - 75,
         y: (this.canvas.height / 2) - 40,
         x_speed: 0,
-        y_speed: 0
+        y_speed: 0,
+        score: 0
       };
     }
   };
@@ -46,7 +60,13 @@ window.onload = function() {
       this.playerOne = Paddle.init.call(this, "left");
       this.playerTwo = Paddle.init.call(this);
       this.ball = Ball.init.call(this);
-      animate(Game.loop);
+
+      ////IF GAME HAS BEGUN
+      this.isRunning = false;
+      ////CONTROL FPS (60FPS)
+      this.fps = 1000 / 60;
+      this.then = Date.now();
+      Game.draw();
     },
     ////DRAW NEW FRAME
     draw: function draw() {
@@ -58,29 +78,62 @@ window.onload = function() {
       this.context.fillStyle = "white";
       this.render(this.playerOne);
       this.render(this.playerTwo);
-      this.render(this.ball);
+      if(this.isRunning) {
+        this.render(this.ball);
+      }
+
+      this.menu();
     },
     ////GAME LOOP
     loop: function loop() {
-      Game.update();
-      Game.draw();
-      animate(Game.loop);
+      Game.winMenu();
+      if(Game.isRunning) {
+        let now = Date.now();
+        let elapsed = now - Game.then;
+        animate(Game.loop);
+        if(elapsed > Game.fps) {
+          Game.then = now - (elapsed % Game.fps);
+          Game.update();
+          Game.draw();
+        }
+      }
+    },
+    ////RENDER MAIN AND SCORE MENU
+    menu: function menu() {
+      this.context.font = "2em Arial";
+      this.context.textAlign = "center";
+
+      if(!this.isRunning) {
+        this.context.fillText("Press arrow key to begin", this.canvas.width / 2, this.canvas.height / 2);
+      }
+      this.context.fillText(this.playerOne.score, this.canvas.width / 3.5, this.canvas.height / 5);
+      this.context.fillText(this.playerTwo.score, this.canvas.width / 1.4, this.canvas.height / 5);
+
+      this.context.font = "1.5em Arial";
+      this.context.fillText(FINAL_SCORE, this.canvas.width / 2, this.canvas.height / 10);
     },
     ////RENDER GAME OBJECTS
     render: function render(gameObject) {
       this.context.fillRect(gameObject.x, gameObject.y, gameObject.width, gameObject.height);
     },
+    ////UPDATE GAME LOGIC
     update: function update() {
       this.ball.x += this.ball.x_speed;
       this.ball.y += this.ball.y_speed;
       ////PLAYER ONE PADDLE COLLISIONS
       if((this.ball.x <= this.playerOne.x + this.playerOne.width) && 
-          (this.ball.x >= this.playerOne.x + this.playerOne.width) &&
+          (this.ball.x >= this.playerOne.x) &&
           (this.ball.y <= this.playerOne.y + this.playerOne.height) &&
           (this.ball.y + this.ball.height >= this.playerOne.y)
         ) {
         this.ball.x_speed = BALL_SPEED_X >= 0 ? BALL_SPEED_X : -BALL_SPEED_X;
         this.ball.x += this.ball.x_speed;
+        if((this.ball.y_speed - this.playerOne.y_speed / 2) <= 1 && (this.ball.y_speed - this.playerOne.y_speed / 2) >= -1) {
+          this.ball.y_speed -= this.playerOne.y_speed;
+        }
+        else {
+          this.ball.y_speed -= (this.playerOne.y_speed / 2); //
+        }
       }
       ////PLAYER TWO PADDLE COLLISIONS
       if((this.ball.x + this.ball.width >= this.playerTwo.x) && 
@@ -90,16 +143,90 @@ window.onload = function() {
         ) {
         this.ball.x_speed = BALL_SPEED_X >= 0 ? -BALL_SPEED_X : BALL_SPEED_X;
         this.ball.x += this.ball.x_speed;
+        if((this.ball.y_speed - this.playerTwo.y_speed / 2) <= 1  && (this.ball.y_speed - this.playerTwo.y_speed / 2) >= -1) {
+          this.ball.y_speed -= this.playerTwo.y_speed;
+        }
+        else {
+          this.ball.y_speed -= (this.playerTwo.y_speed / 2);
+        }
       }
-      ////COLLISION WITH TOP WALL
+      ////BALL COLLISION WITH TOP WALL
       if(this.ball.y <= 0) {
-        this.ball.y_speed = BALL_SPEED_Y >= 0 ? BALL_SPEED_Y : -BALL_SPEED_Y;
+        this.ball.y_speed = -(this.ball.y_speed  / BALL_DEACCELERATION);
         this.ball.y += this.ball.y_speed;
       }
-      ////COLLISION WITH BOTTOM WALL
+      ////BALL COLLISION WITH BOTTOM WALL
       if(this.ball.y + this.ball.height >= this.canvas.height) {
-        this.ball.y_speed = BALL_SPEED_Y >= 0 ? -BALL_SPEED_Y : BALL_SPEED_Y;
+        this.ball.y_speed = -(this.ball.y_speed / BALL_DEACCELERATION);
         this.ball.y += this.ball.y_speed;
+      }
+      ////HANDLE PLAYER ONE SCORE
+      if(this.ball.x >= this.canvas.width) {
+        this.playerOne.score += 1;
+        this.ball = this.playerOne.score === FINAL_SCORE ? this.ball : Ball.init.call(this);
+        this.ball.x_speed = BALL_SPEED_X >= 0 ? BALL_SPEED_X : -BALL_SPEED_X;
+      }
+      ////HANDLE PLAYER TWO SCORE
+      if(this.ball.x <= 0) {
+        this.playerTwo.score += 1;
+        this.ball = this.playerTwo.score === FINAL_SCORE ? this.ball : Ball.init.call(this);
+        this.ball.x_speed = BALL_SPEED_X >= 0 ? -BALL_SPEED_X : BALL_SPEED_X;
+      }
+      ////PLAYER ONE MOVEMENT
+      if(isKeyDown) {
+        this.playerOne.y -= this.playerOne.y_speed;
+      }
+      ////COMPUTER AI MOVEMENT
+      if((this.playerTwo.y + this.playerTwo.height / 2) < (this.ball.y + this.ball.height / 2)) {
+        this.playerTwo.y += COMP_PADDLE_SPEED_Y;
+      }
+      if((this.playerTwo.y + this.playerTwo.height / 2) > (this.ball.y + this.ball.height / 2)) {
+        this.playerTwo.y -= COMP_PADDLE_SPEED_Y;
+      }
+      if(FINAL_SCORE / 2 <= this.playerOne.score && !COMP_CHALLENGE) {
+        COMP_CHALLENGE = true;
+        COMP_PADDLE_SPEED_Y = COMP_PADDLE_SPEED_Y * COMP_PADDLE_SPEED_Y_MULTIPLYER;
+      }
+      ////PLAYER ONE COLLISION WITH TOP WALL
+      if(this.playerOne.y <= 0) {
+        this.playerOne.y = 0;
+      }
+      ////PLAYER ONE COLLISION WITH BOTTOM WALL
+      if(this.playerOne.y + this.playerOne.height >= this.canvas.height) {
+        this.playerOne.y = this.canvas.height - this.playerOne.height;
+      }
+      ////PLAYER TWO COLLISION WITH TOP WALL
+      if(this.playerTwo.y <= 0) {
+        this.playerTwo.y = 0;
+      }
+      ////PLAYER TWO COLLISION WITH BOTTOM WALL
+      if(this.playerTwo.y + this.playerTwo.height >= this.canvas.height) {
+        this.playerTwo.y = this.canvas.height - this.playerTwo.height;
+      }
+    },
+    ////RENDER WIN MENU
+    winMenu: function winMenu() {
+      let whoWon = null;
+      if(this.playerOne.score === FINAL_SCORE) {
+        whoWon = "ONE";
+      }
+      if(this.playerTwo.score === FINAL_SCORE) {
+        whoWon = "TWO";
+      }
+      if(whoWon) {
+        this.context.font = "2em Arial";
+        this.context.textAlign = "center";
+        this.context.fillText("PLAYER " + whoWon + " WINS!", this.canvas.width / 2, this.canvas.height / 2);
+
+        this.context.font = "1.2em Arial";
+        this.context.textAlign = "center";
+        this.context.fillText("Press spacebar to play again!", this.canvas.width / 2, this.canvas.height / 1.8);
+        if(COMP_CHALLENGE) {
+          COMP_CHALLENGE = false;
+          COMP_PADDLE_SPEED_Y = COMP_PADDLE_SPEED_Y / COMP_PADDLE_SPEED_Y_MULTIPLYER;
+        }
+        Game.isRunning = false;
+        GAME_FINISH = true;
       }
     }
   };
@@ -107,4 +234,56 @@ window.onload = function() {
   ////INSTANTIATE GAME AND START
   Game.init();
   Game.draw();
+
+  ////LISTEN FOR PLAYER CONTROLS
+  window.addEventListener("keydown", function(e) {
+    ////RESTART GAME WHEN FINISHED
+    if(e.keyCode === 32 && GAME_FINISH) {
+      GAME_FINISH = false;
+      Game.init();
+      Game.draw();
+    }
+    ////START GAME WITH ARROW KEYS OR WASD
+    if((e.keyCode === 87 || e.keyCode === 38 || e.keyCode === 83 || e.keyCode === 40) && !Game.isRunning) {
+      Game.isRunning = true;
+      animate(Game.loop);
+    }
+    ////HANDLE UP ARROW AND W KEY
+    if(e.keyCode === 87 || e.keyCode === 38) {
+      ////PREVENT HELD KEYDOWN SPAM
+      if(keysDown.length && (keysDown[keysDown.length - 1].key === e.keyCode)) {
+        return;
+      }
+      keysDown.push({key: e.keyCode, speed: PADDLE_SPEED_Y >= 0 ? PADDLE_SPEED_Y : -PADDLE_SPEED_Y});
+      isKeyDown = true;
+      Game.playerOne.y_speed = keysDown[keysDown.length - 1].speed;
+    }
+    ////HANDLE DOWN ARROW AND S KEY
+    if(e.keyCode === 83 || e.keyCode === 40) {
+      ////PREVENT HELD KEYDOWN SPAM
+      if(keysDown.length && (keysDown[keysDown.length - 1].key === e.keyCode)) {
+        return;
+      }
+      keysDown.push({key: e.keyCode, speed: PADDLE_SPEED_Y >= 0 ? -PADDLE_SPEED_Y : PADDLE_SPEED_Y});
+      isKeyDown = true;
+      Game.playerOne.y_speed = keysDown[keysDown.length - 1].speed;
+    }
+  });
+  ////LISTEN FOR LET GO PLAYER CONTROLS
+  window.addEventListener("keyup", function(e) {
+    if(keysDown.length && (e.keyCode === 87 || e.keyCode === 38 || e.keyCode === 83 || e.keyCode === 40)) {
+      keysDown = keysDown.filter(key => {
+        return key.key !== e.keyCode;
+      });
+      if(keysDown.length) {
+        Game.playerOne.y_speed = keysDown[keysDown.length - 1].speed;
+        return;
+      }
+      isKeyDown = false;
+      ////MAKE PADDLE PHYSICS MORE REALISTIC BY NOT HAVING Y MOMENTUM WHEN STAYING STILL
+      if(REALISTIC_PADDLE) {
+        Game.playerOne.y_speed = 0;
+      }
+    }
+  });
 };
